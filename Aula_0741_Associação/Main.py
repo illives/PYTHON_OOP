f8078630dfc32c9439c62d934e1eb9c9abfe1854
illives <illives9455@hotmail.com>
2021-07-19 11:15:25 -0300

    folder_adjust

--- Aula_0741_Associação/Main.py
+++ Aula_0741_Associação/Main.py
@@ -5,9 +5,9 @@ escritor = Escritor ('João Antonio')
 caneta = Caneta ('BIC')
 maquina = MaquinaEscrever()
 
-# O Atibuto ferramenta vai receber um objeto da classe Caneta ou Maquina de escrever
+# O Atibuto ferramenta da classe Escritor vai receber um objeto da classe Caneta ou Maquina de escrever
 #No caso abaixo irá receber o metodo escrever desta classe
-escritor.ferramenta = maquina
+escritor.ferramenta = maquina #escritor.ferramenta recebe objeto maquina
 escritor.ferramenta.escrever()
 escritor.ferramenta = caneta
 escritor.ferramenta.escrever()
